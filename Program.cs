107a2697622e64153d96b30327e817d232f25211
BrazilBatres <60472938+BrazilBatres@users.noreply.github.com>
2023-03-26 15:13:33 -0600

    Ejercicio 1 HT

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Autofac.Extensions.DependencyInjection;
 using chessAPI;
 using chessAPI.business.interfaces;
 using chessAPI.models.player;
+using chessAPI.models.game;
 using Microsoft.AspNetCore.Authorization;
 using Serilog;
 using Serilog.Events;
@@ -38,13 +39,37 @@ try
     var app = builder.Build();
     app.UseSerilogRequestLogging();
     app.UseMiddleware(typeof(chessAPI.customMiddleware<int>));
-    app.MapGet("/", () =>
+
+    #region "REST routes"
+    app.MapGet("/", () => Results.BadRequest());
+
+    #region "Player REST Commands"
+    app.MapGet("game/{id}", async (IGameBusiness bs, long id) =>
     {
-        return "hola mundo";
+        var x = await bs.getGame(id).ConfigureAwait(false);
+        return x != null ? Results.Ok(x) : Results.NotFound();
     });
 
-    app.MapPost("player", 
-    [AllowAnonymous] async(IPlayerBusiness<int> bs, clsNewPlayer newPlayer) => Results.Ok(await bs.addPlayer(newPlayer)));
+    app.MapPost("player",
+    [AllowAnonymous] async (IPlayerBusiness<int> bs, clsNewPlayer newPlayer)
+        => Results.Ok(await bs.addPlayer(newPlayer).ConfigureAwait(false)));
+    #endregion
+
+    #region "Game REST Commands"
+    app.MapPost("game",
+    [AllowAnonymous] async (IGameBusiness bs, clsNewGame newGame) =>
+    {
+        await bs.startGame(newGame).ConfigureAwait(false);
+        return Results.Ok();
+    });
+    app.MapPut("/game/{id}/swapturn",
+    [AllowAnonymous] async (IGameBusiness bs, long id) =>
+    {
+        var didSwap = await bs.swapTurn(id).ConfigureAwait(false);
+        return didSwap ? Results.Ok() : Results.BadRequest();
+    });
+    #endregion
+    #endregion
 
     app.Run();
 }
